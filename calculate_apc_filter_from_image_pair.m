d06095e099d3993bcd9598ee9d8325fa9761a2c6
Matthew Giarra <matthew.giarra@gmail.com>
2016-10-03 10:16:25 -0400

    calculate_apc_filter_from_image_pair_02.m to calculate_apc_filter_from_image_pair.m

--- calculate_apc_filter_from_image_pair.m
+++ calculate_apc_filter_from_image_pair.m
@@ -1,204 +1,140 @@
-function [SPECTRAL_FILTER, PARTICLE_SHAPE, FILTER_FIT] = calculate_apc_filter_from_image_pair(image_01, image_02, ...
-    grid_y, grid_x, region_height, region_width, window_fraction, shuffle_range, shuffle_step, num_regions_neq);
-
+function [APC_STD_Y, APC_STD_X, APC_FILTER] = ...
+    calculate_apc_filter_from_image_pair(image_01, image_02, ...
+    grid_y, grid_x, region_size, window_fraction, shuffle_range, shuffle_step)
+% APC_STD_Y, APC_STD_X, APC_FILTER] = ...
+%     calculate_apc_filter_from_image_pair(image_01, image_02, ...
+%     grid_y, grid_x, region_size, window_fraction, shuffle_range, shuffle_step)
+% 
+% CALCULATE_APC_FILTER_FROM_IMAGE_PAIR
 
 % % Extract grid coordinates
 gy = grid_y(:);
 gx = grid_x(:);
 
 % Number of regions
-num_regions_eq = length(gy);
-
-% Make the Gaussian window
-g_win = gaussianWindowFilter([region_height, region_width], ...
-    window_fraction, 'fraction');
-
-
-% Calculate the NCC
 num_regions = length(gy);
 
-% Augment the list by a lot so we can get rid of the equal-region pairs
-% and still have plenty left over
-% List of the first random correlation regions
-region_numbers_neq_aug_01 = round((num_regions - 1) * rand(2 * num_regions_neq, 1) + 1);
+% Size of the region
+region_height = region_size(1);
+region_width = region_size(2);
 
-% List of the second random correlation regions
-region_numbers_neq_aug_02 = round((num_regions - 1) * rand(2 * num_regions_neq, 1) + 1);
+% Make the Gaussian window
+% and save it for later.
+g_win = gaussianWindowFilter(...
+    [region_height, region_width], ...
+    window_fraction, 'fraction');
 
-% Find the equivalent indices
-equiv_inds = find(region_numbers_neq_aug_01 == region_numbers_neq_aug_02);
-
-% Pop the equivalent indices
-region_numbers_neq_aug_01(equiv_inds) = [];
-region_numbers_neq_aug_02(equiv_inds) = [];
-
-% Take the first num_ens_neq of regions
-region_numbers_neq_01 = region_numbers_neq_aug_01(1 : num_regions_neq);
-region_numbers_neq_02 = region_numbers_neq_aug_02(1 : num_regions_neq);
-
-% % % % % shuffle_range_x = shuffle_range;
-% % % % % shuffle_step_x = shuffle_step;
-% % % % % 
-% % % % % shuffle_range_y = shuffle_range;
-% % % % % shuffle_step_y = shuffle_step;
-% % % % % 
-% % % % % % Center pixels
-% % % % % xc = (region_width  + 1) / 2 + 0.5 * (1 - mod(region_width,  2));
-% % % % % yc = (region_height + 1) / 2 + 0.5 * (1 - mod(region_height, 2));
-
-% Allocate the NCC sum
-ncc_sum = zeros(region_height, region_width) ...
-    + 1i * zeros(region_height, region_width);
-
-% Allocate the particle shape sum
-particle_shape_sum = zeros(region_height, region_width) ...
-    + 1i * zeros(region_height, region_width);
-
-for k = 1 : num_regions_neq
-
-    % Inform the user
-    fprintf(1, 'NCC %d of %d\n', k, num_regions_neq);
-
-    % % Extract the region from the first image
-    region_neq_01 = extractSubRegions(double(image_01), ...
-        [region_height, region_width], gx(region_numbers_neq_01(k)), gy(region_numbers_neq_01(k)));
-
-    % Extract the region from the second image
-    region_neq_02 = extractSubRegions(double(image_02), ...
-        [region_height, region_width], gx(region_numbers_neq_02(k)), gy(region_numbers_neq_02(k)));
-
-    % Transforms
-    F1_neq = fft2(g_win .* (region_neq_01 - mean(region_neq_01(:))));
-    F2_neq = fft2(g_win .* (region_neq_02 - mean(region_neq_02(:))));
-
-    % Autocorrelations
-    ac_01_neq = fftshift(F1_neq .* conj(F1_neq));
-    ac_02_neq = fftshift(F2_neq .* conj(F2_neq));
-
-    % Average auto correlationd
-    ac_mean = (ac_01_neq + ac_02_neq) / 2;
-
-    % Cross correlation
-    ncc_cur = fftshift(F1_neq .* conj(F2_neq));
-    
-    % Particle shape current
-    particle_shape_cur = abs(ac_mean);
-   
-    % Sum
-    ncc_sum = ncc_sum + ncc_cur;
-   
-    % Sum particle shape
-    particle_shape_sum = particle_shape_sum + particle_shape_cur;
-
-
+% Default to a single shuffle range.
+if length(shuffle_range) == 1
+    shuffle_range_y = shuffle_range;
+    shuffle_range_x = shuffle_range;
+else
+    shuffle_range_y = shuffle_range(1);
+    shuffle_range_x = shuffle_range(2);
 end
 
-% Normalize the particle so its maximum is one.
-% Note that we don't want to force the minimum to zero
-% Because that will cause the edges to blow up
-% whenever we divide by it.
-particle_shape_norm = real(particle_shape_sum ./ max(particle_shape_sum(:)));
-
-% Allocate the CC sums
-cc_abs_sum = zeros(region_height, region_width);
-cc_abs_full_sum = zeros(region_height, region_width);
+% Default to a single shuffle step.
+if length(shuffle_step) == 1
+    shuffle_step_y = shuffle_step;
+    shuffle_step_x = shuffle_step;
+else
+    shuffle_step_y = shuffle_step(1);
+    shuffle_step_x = shuffle_step(2);
+end
 
 % Shake-the-box shfting coordinates
-gx_shift = -1 * shuffle_range : shuffle_step : shuffle_range;
-gy_shift = -1 * shuffle_range : shuffle_step : shuffle_range;
+gx_shift = -1 * shuffle_range_x : shuffle_step_x : shuffle_range_x;
+gy_shift = -1 * shuffle_range_y : shuffle_step_y : shuffle_range_y;
 
+% Default to not shifting
+% the regions (columns)
 if isempty(gx_shift)
     gx_shift = 0;
 end
 
+% Default to not shifting
+% the regions (rows)
 if isempty(gy_shift)
     gy_shift = 0;
 end
 
-[GX, GY] = meshgrid(gx_shift, gy_shift);   
-    
-
-cc_sum_full = zeros(region_height, region_width) + ...
-    1i * zeros(region_height, region_width);
+% Make a grid of the shuffle points
+% These are the locations to which
+% each grid point will be shifted 
+% ("shuffled"), and the origin
+% of these shifts is specified
+% as the grid point itself.
+[shuffle_X, shuffle_Y] = meshgrid(gx_shift, gy_shift);   
+   
+% Allocate vectors to hold the
+% paramters of the Gaussian
+% functions that will be fit
+% to the magnitudes of
+% the correlations.
+APC_STD_X = zeros(num_regions, 1);
+APC_STD_Y = zeros(num_regions, 1);
 
 % Do the corresponding correlation
-for k = 1 : num_regions_eq
+for k = 1 : num_regions
     
-    fprintf(1, 'CCC region %d of %d\n', k, num_regions_eq);
+    % Inform the user.
+    fprintf(1, 'On region %d of %d\n', k, num_regions);
     
-    % Allocate running CC sum for the CCC
+    % Allocate running correlation sum.
+    % This is re-zeroed for each region
+    % because it is the magnitude of 
+    % this complex array that is taken
+    % to be the APC filter, and a 
+    % different filter is calculated for each region!
     cc_sum = zeros(region_height, region_width) + ...
-    1i * zeros(region_height, region_width);
+        1i * zeros(region_height, region_width);
    
     % Shake-the-box
-    for p = 1 : length(GX(:))
+    for p = 1 : length(shuffle_X(:))
         
-        shifted_grid_x = gx(k) + GX(p);
-        shifted_grid_y = gy(k) + GY(p);
+        % Shift the grid (this is the shuffle)
+        % The Kobayashi Shake of PIV
+        % The 
+        shifted_grid_x = gx(k) + shuffle_X(p);
+        shifted_grid_y = gy(k) + shuffle_Y(p);
         
-        region_eq_01 = extractSubRegions(image_01,...
+        % Extract the subregions.
+        region_01 = extractSubRegions(image_01,...
             [region_height, region_width], shifted_grid_x, shifted_grid_y);
-        region_eq_02 = extractSubRegions(image_02,...
+        region_02 = extractSubRegions(image_02,...
             [region_height, region_width], shifted_grid_x, shifted_grid_y);
 
         % Transforms
-        F1_eq = fft2(g_win .* (region_eq_01 - mean(region_eq_01(:))));
-        F2_eq = fft2(g_win .* (region_eq_02 - mean(region_eq_02(:))));
+        F1_eq = fft2(g_win .* (region_01 - mean(region_01(:))));
+        F2_eq = fft2(g_win .* (region_02 - mean(region_02(:))));
 
         % Cross correlation
         cc_cur = fftshift(F1_eq .* conj(F2_eq));
 
-        % Divide by the particle shape
-        cc_eq = cc_cur ./ particle_shape_norm;
-
-        % Ensemble corresponding correlation
-        % (divided by particle shape, minus NCC)
-        cc_sum = cc_sum + cc_eq;
+        % Ensemble correlation
+        cc_sum = cc_sum + cc_cur;
         
-        cc_sum_full = cc_sum_full + cc_cur;
-       
     end
     
-    % Add the sum of that trial to the sum-over-trials.
-    % This means we're taking the magnitude after summing.
-    cc_abs_sum = cc_abs_sum + (abs(cc_sum)).^2;
+    % Take the magnitude of
+    % the summed correlation. 
+    cc_mag = abs(cc_sum);
     
+    % Fit a Gaussian to this
+    % since, let's be honest, 
+    % it's probably Gaussian...
+    % Because what isn't these days
+    [~, APC_STD_Y(k), APC_STD_X(k), ~, ~, filter_offset, filter_raw] =...
+    fit_gaussian_2D(cc_mag);
+    
+    % Subtract the minimum from the fit
+    filter_sub = filter_raw - filter_offset;
+  
+    % Divide by the max so that the
+    % range of the filter is [0, 1]
+    APC_FILTER = filter_sub - max(filter_sub(:));
+      
 end
-
-% Square root of the abs sum
-cc_abs_sum_sqrt = abs(sqrt(cc_abs_sum));
-
-% Subtract the minimum
-cc_abs_sum_sub = cc_abs_sum_sqrt - min(cc_abs_sum_sqrt(:));
-
-% Normalize
-cc_abs_sum_norm = cc_abs_sum_sub ./ max(cc_abs_sum_sub(:));
-
-% Min subtracted particle shape
-particle_shape_sub = particle_shape_norm - min(particle_shape_norm(:));
-
-% Normalized particle shape
-PARTICLE_SHAPE = particle_shape_sub ./ max(particle_shape_sub(:));
-
-% Full filter;
-spectral_filter_full = cc_abs_sum_norm .* PARTICLE_SHAPE;
-
-% Min subtracted spectral filter.
-spectral_filter_sub = (spectral_filter_full - min(spectral_filter_full(:)));
-
-% Normalized spectral filter
-SPECTRAL_FILTER = spectral_filter_sub ./ max(spectral_filter_sub(:));
-
-% Fit a Gaussian to this filter.
-% A lot of PIV things will be Gaussian.
-[~, ~, ~, ~, ~, array_offset, array_full] =...
-    fit_gaussian_2D(SPECTRAL_FILTER);
-
-% Min subtracted filter fit
-array_sub = array_full - array_offset;
-
-% Normalized filter fit.
-FILTER_FIT = array_sub ./ max(array_sub(:));
 
 
 end
