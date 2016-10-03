a630468ba66422a0acd51c54cd2683061e973cf7
Matthew Giarra <matthew.giarra@gmail.com>
2016-10-03 11:03:21 -0400

    Added header desription to calculate_apc_filter_from_image_pair.m and removed outputting the full filter.

--- calculate_apc_filter_from_image_pair.m
+++ calculate_apc_filter_from_image_pair.m
@@ -5,7 +5,65 @@ function [APC_STD_Y, APC_STD_X, APC_FILTER] = ...
 %     calculate_apc_filter_from_image_pair(image_01, image_02, ...
 %     grid_y, grid_x, region_size, window_fraction, shuffle_range, shuffle_step)
 % 
-% CALCULATE_APC_FILTER_FROM_IMAGE_PAIR
+% CALCULATE_APC_FILTER_FROM_IMAGE_PAIR calculates the standard deviations
+% of Gaussian-shaped spectral filters from a pair of
+% images at a list of grid points. 
+%
+% INPUTS
+%     image_01: [M x N] array containing the image data
+%         the first image in the pair.
+% 
+%     image_02: [M x N] array containing the image data
+%         the second image in the pair.
+% 
+%     grid_y: Array or vector containing the pixel
+%         row coordinates of the interrogation regions
+%         at which the filters will be calculated.
+% 
+%     grid_x: Array or vector containing the pixel
+%         column coordinates of the interrogation regions
+%         at which the filters will be calculated.
+% 
+%     region_size: [2 x 1] vector specifying the
+%         height and width of the interrogation regions
+%         in pixels.
+%         region_size = [region_size_rows, region_size_columns];
+% 
+%     window_fraction: [2 x 1] vector specifying the 
+%         fractional height and width of the interrogation
+%         regions' apodization window. Each element is between 0 and 1.
+%         0 specifies the image is completely apodized in that direction
+%         and 1 specifies the image is un-apodized. 
+%         window_fraction = [window_fraction_rows, window_fraction_cols];
+% 
+%     shuffle_range: [2 x 1] vector specifying the row and column 
+%         range across which each interrogation is shifted for calculating
+%         the filter. This is the range of the shake-the-box analog for this
+%         method. 
+%         shuffle_range = [shuffle_range_rows, shuffle_rows_cols];
+% 
+%     shuffle_step: [2 x 1] vector specifying the row and column 
+%         spacing between shifted locations of regions for calculating
+%         the filter. This is the spacing of the of the shake-the-box
+%         analog for this method. 
+%         shuffle_step = [shuffle_step_rows, shuffle_step_cols];
+% 
+% OUTPUTS
+%     APC_STD_Y = Vector containing the standard deviations
+%         of the APC filters in the row direction,
+%         located at each grid point.
+%         The order of this list of values corresponds 
+%         to the order of the grid points located
+%         at [grid_y(:), grid_x(:)].
+%
+%     APC_STD_X = Vector containing the standard deviations
+%         of the APC filters in the column direction,
+%         located at each grid point.
+%         The order of this list of values corresponds 
+%         to the order of the grid points located
+%         at [grid_y(:), grid_x(:)].
+%
+%
 
 % % Extract grid coordinates
 gy = grid_y(:);
@@ -124,16 +182,9 @@ for k = 1 : num_regions
     % since, let's be honest, 
     % it's probably Gaussian...
     % Because what isn't these days
-    [~, APC_STD_Y(k), APC_STD_X(k), ~, ~, filter_offset, filter_raw] =...
+    [~, APC_STD_Y(k), APC_STD_X(k)] =...
     fit_gaussian_2D(cc_mag);
     
-    % Subtract the minimum from the fit
-    filter_sub = filter_raw - filter_offset;
-  
-    % Divide by the max so that the
-    % range of the filter is [0, 1]
-    APC_FILTER = filter_sub - max(filter_sub(:));
-      
 end
 
 
